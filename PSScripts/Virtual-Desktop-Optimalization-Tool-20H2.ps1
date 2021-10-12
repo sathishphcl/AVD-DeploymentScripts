a202ae2e2ea9de686dbd5a095994e8ce2861bbbd
Ivo Uenk <mail@udirection.com>
2021-10-12 17:01:58 +0200

    s

--- PSScripts/Virtual-Desktop-Optimalization-Tool-20H2.ps1
+++ PSScripts/Virtual-Desktop-Optimalization-Tool-20H2.ps1
@@ -1,7 +1,7 @@
 $path ="C:\Packages\"
 $ErrorActionPreference = 'SilentlyContinue'
 
-$OptimalizationToolURL="https://github.com/iuenk/WVD/blob/main/Resources/Virtual-Desktop-Optimization-Tool-custom-20h2.zip?raw=true"
+$OptimalizationToolURL="https://github.com/iuenk/AVD/blob/main/Resources/Virtual-Desktop-Optimization-Tool-custom-20h2.zip?raw=true"
 $installerFile="Virtual-Desktop-Optimization-Tool-custom-20h2.zip"
 
 mkdir $path -ErrorAction SilentlyContinue
