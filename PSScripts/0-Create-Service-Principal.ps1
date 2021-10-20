7c80434f82edb9c1f620f494e481e2f3f2d29c64
Ivo Uenk <mail@udirection.com>
2021-10-20 22:03:38 +0200

    s

--- PSScripts/0-Create-Service-Principal.ps1
+++ PSScripts/0-Create-Service-Principal.ps1
@@ -31,13 +31,36 @@ New-AzRoleAssignment -ObjectId $spid -RoleDefinitionName "Desktop Virtualization
 New-AzRoleAssignment -ObjectId $spid -RoleDefinitionName "Storage Blob Data Contributor" -Scope /subscriptions/$subscriptionID
 
 ##################################### NOTICE ##########################################################################
+# Copy the output and place it somewhere safe! You cannot get this info later!
+Write-Host "You can now add the Azure Resource Manager connecting string and connect the Azure Vault in Azure DevOps, use informatie below"
+$jsonresp | ConvertTo-Json
+
+##################################### NOTICE ##########################################################################
 
 # Assign Graph API app registration rights otherwise Get-AzRoleAssignment in 2-Create-AIB-Identity will not work
 # It's a bug see info https://github.com/Azure/azure-powershell/issues/13573
 # Azure Active Directory Graph -> Directory.Read.All Application
 # Microsoft Graph -> Directory.Read.All Application
 
-##################################### NOTICE ##########################################################################
-# Copy the output and place it somewhere safe! You cannot get this info later!
-Write-Host "You can now add the Azure Resource Manager connecting string and connect the Azure Vault in Azure DevOps, use informatie below"
-$jsonresp | ConvertTo-Json
+# It's not posible to assign the Azure Active Directory Graph from the GUI use code below to add permissions
+# You still need to grant permissions after adding them
+# A full refresh of the portal is needed before you can see the new permissions
+$AppObjectID = "<AppObjectID>" # object id from Service Principal Name
+$app = Get-AzureADApplication -ObjectId $AppObjectID
+$AADAccess = $app.RequiredResourceAccess | Where-Object {$_.ResourceAppId -eq "00000002-0000-0000-c000-000000000000"}  # "00000002-0000-0000-c000-000000000000" represents AAD Graph API
+if($AADAccess -eq $null) {
+              $AADAccess = New-Object Microsoft.Open.AzureAD.Model.RequiredResourceAccess
+              $AADAccess.ResourceAppId = "00000002-0000-0000-c000-000000000000"
+              $Access = New-Object Microsoft.Open.AzureAD.Model.ResourceAccess
+              $Access.Type = "Role" # Scope is delgated, Role is application.
+              $Access.Id = "5778995a-e1bf-45b8-affa-663a9f3f4d04" # Directory.Read.All
+              $AADAccess.ResourceAccess = @()
+              $AADAccess.ResourceAccess.Add($Access)
+          $app.RequiredResourceAccess.Add($AADAccess)
+} else {
+              $Access = New-Object Microsoft.Open.AzureAD.Model.ResourceAccess
+              $Access.Type = "Role" # Scope is delgated, Role is application.
+              $Access.Id = "5778995a-e1bf-45b8-affa-663a9f3f4d04" # Directory.Read.All
+              $AADAccess.ResourceAccess.Add($Access)
+}
+Set-AzureADApplication -ObjectId $AppObjectID -RequiredResourceAccess $app.RequiredResourceAccess
