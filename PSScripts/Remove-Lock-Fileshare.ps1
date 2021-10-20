a9d1daa018f04534e43b611b0a53c68d2ab088f2
Ivo Uenk <ivo.uenk@solvinity.com>
2021-10-20 08:41:51 +0200

    s

--- PSScripts/Remove-Lock-Fileshare.ps1
+++ PSScripts/Remove-Lock-Fileshare.ps1
@@ -1,3 +1,6 @@
 $Context = New-AzStorageContext -StorageAccountName "ucorpwvdprem" -StorageAccountKey "<StorageAccountKey>"
 Get-AzStorageFileHandle -Context $Context -ShareName "fslogixoffice" -Recursive
 Close-AzStorageFileHandle -Context $Context -ShareName "fslogixoffice" -CloseAll
+
+# Close specific path
+Close-AzStorageFileHandle -Context $Context -ShareName "fslogixoffice" -Path "uenki_S-1-5-21-1951289647-149518158-1539857752-24793/ODFC_uenki.VHDX" -CloseAll
