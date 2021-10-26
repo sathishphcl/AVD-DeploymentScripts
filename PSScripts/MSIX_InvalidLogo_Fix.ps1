4540b736ff4d1edd41e3953df921813a2974c33e
Ivo Uenk <mail@udirection.com>
2021-10-26 09:00:51 +0200

    s

--- PSScripts/MSIX_InvalidLogo_Fix.ps1
+++ PSScripts/MSIX_InvalidLogo_Fix.ps1
@@ -3,5 +3,5 @@
 .\makepri.exe dump /if 'C:\Users\Ivo\Desktop\Resources.pri'
 code .\resources.pri.xml
 
-#Verwijder handmatig even:
+# Remove manually
 C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x86\resources.pri.xml
