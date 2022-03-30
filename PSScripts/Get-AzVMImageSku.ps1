1e876b8ff3aa2f75d394d53e06d8e6a29c3263e2
Ivo Uenk <mail@udirection.com>
2022-03-30 22:33:00 +0200

    s

--- PSScripts/Get-AzVMImageSku.ps1
+++ PSScripts/Get-AzVMImageSku.ps1
@@ -1,4 +1,4 @@
-Get-AzVMImagePublisher -Location "West Europe" | Select PublisherName | Where-Object { $_.PublisherName -like '*Windows*' }
+Get-AzVMImagePublisher -Location "West Europe" | Select-Object PublisherName | Where-Object { $_.PublisherName -like '*Windows*' }
 
 # Listing Image Offers
 $Publisher = "MicrosoftWindowsDesktop"
@@ -7,7 +7,7 @@ $Publisher = "MicrosoftWindowsDesktop"
 Get-AzVMImageOffer -Location "West Europe" -PublisherName $publisher
 
 # Listing Image SKUs
-# office-365
-# Windows-10
-$Offer = "Windows-10"
-Get-AzVMImageSku -Location "West Europe" -PublisherName $Publisher -Offer $Offer | Select Skus
+# office-365 (multi-session AVD)
+# Windows-10 (personal AVD)
+$Offer = "office-365"
+Get-AzVMImageSku -Location "West Europe" -PublisherName $Publisher -Offer $Offer | Select-Object Skus
