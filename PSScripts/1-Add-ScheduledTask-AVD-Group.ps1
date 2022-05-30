003d64e9c84c96543dcb2817e34e250302854193
Ivo Uenk <mail@udirection.com>
2022-05-30 22:06:17 +0200

    s

--- PSScripts/1-Add-ScheduledTask-AVD-Group.ps1
+++ PSScripts/1-Add-ScheduledTask-AVD-Group.ps1
@@ -1,21 +1,3 @@
-# Create script
-$TargetGroup = "SG_AVD_Hosts"
-$TargetOU = "OU=AVD,OU=Ucorp,DC=ucorp,DC=local"
-
-$AVDHosts = Get-ADComputer -Filter * | Where-Object {$_.DistinguishedName -like "*$TargetOU*" -and $_.Enabled -eq "True"}
-
-ForEach($AVDHost in $AVDHosts)
-{
-    $HostName = $AVDHost.Name
-
-    $Membership = Get-ADGroup $TargetGroup | Get-ADGroupMember | Where-Object {$_.Name -eq $HostName}
-    if(!$Membership)
-    {
-    "Adding $HostName to $TargetGroup"
-    Get-ADGroup $TargetGroup | Add-ADGroupMember -Members $AVDHost -Verbose
-    }
-}
-
 # Get username and password
 $cred = Get-Credential -Message "Enter credentials"
 $username = $cred.username
@@ -24,7 +6,7 @@ $password = $cred.getnetworkcredential().password
 # Parameters for the scheduled task
 $taskFolder = 'Ucorp'
 $taskName = 'Ucorp-AVD-Add-Hosts-To-Group'
-$scriptPath = "\\ucorp.local\SYSVOL\ucorp.local\scripts\Ucorp-AVD-Add-Host-Group.ps1"
+$scriptPath = "\\ucorp.local\SYSVOL\ucorp.local\scripts\AVD-Add-Host-Group.ps1"
 
 $A = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-executionpolicy bypass -noprofile -file $scriptPath" 
 $T = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10)
@@ -33,3 +15,4 @@ $exists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskname}
 If($exists) {Unregister-ScheduledTask -TaskName $taskname -Confirm}
 Register-ScheduledTask -TaskName "$TaskFolder\$TaskName" -Action $A -Trigger $T -RunLevel Highest -User $username -Password $password -ErrorAction Stop
 $cred = ""
+
